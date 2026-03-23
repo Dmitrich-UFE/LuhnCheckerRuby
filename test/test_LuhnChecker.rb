37c968e80441fe4244f15667b9fe5288e8ed4cb4
Евтухов Дмитрий <justarcane@vk.com>
2026-03-23 15:43:48 +0300

    Добавлены тесты на проверку длин номеров карт меньше нужного

--- test/test_LuhnChecker.rb
+++ test/test_LuhnChecker.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require_relative '../LuhnChecker'
 
 class TestLuhnChecker < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::LuhnChecker::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_lengthOfBankCardNumber_tooShort
+    refute IsValidBankCardNumber(45003452123)
+    refute IsValidBankCardNumber(18262879)
+    refute IsValidBankCardNumber(2)
+    refute IsValidBankCardNumber(0)
+    refute IsValidBankCardNumber
   end
 end
