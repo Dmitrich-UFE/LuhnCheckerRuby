70af48e9680790a8b887e8d3c18989329600e1c9
Евтухов Дмитрий <justarcane@vk.com>
2026-03-23 15:00:21 +0300

    Добавлена реализация метода для проверки номера банковской карты

--- lib/LuhnChecker.rb
+++ lib/LuhnChecker.rb
@@ -3,22 +3,34 @@
 require_relative "LuhnChecker/version"
 
 module LuhnCheckerLib
-  VERSION = '0.1.0'
 
   #Checks validity of bank card number
-  def CheckBankCardNumber(bankcardnumber)
-    nil
+  def IsValidBankCardNumber?(bankcardnumber)
+    digits = bankcardnumber.to_s.gsub(/\D/, '').chars.map(&:to_i)
+    return false if digits.empty?
+  
+    check_sum = digits.pop
+    sum = digits.reverse.each_with_index do |digit, index|
+      if index.even?
+        digit *= 2
+        digit -= 9 if digit > 9
+      else
+        digit
+      end
+    end
+  
+    calculated_checksum = (10 - (sum % 10)) % 10
+    check_sum == calculated_checksum
   end
   
   #Checks validity of IMEI
-  def CheckIMEI(imei)
+  def IsValidIMEI?(imei)
     nil
   end
 
   #Checks validity of ICCID
-  def CheckICCID(iccID)
+  def IsValidICCID?(iccID)
     nil
   end
-
 
 end
