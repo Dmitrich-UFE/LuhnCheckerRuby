2c22e5fff7a4180720aef0b25d03487f722253df
Евтухов Дмитрий <justarcane@vk.com>
2026-03-23 11:51:27 +0300

    создан проект

---
+++ lib/LuhnChecker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "LuhnChecker/version"
+
+module LuhnChecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/LuhnChecker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LuhnChecker
+  VERSION = "0.1.0"
+end
