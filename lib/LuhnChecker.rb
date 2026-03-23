70630e50bc718ab0bbc2f7e935d59aa9a430f7de
Евтухов Дмитрий <justarcane@vk.com>
2026-03-23 13:32:39 +0300

    Подправлено нахождение файла модуля

--- lib/LuhnChecker.rb
+++ lib/LuhnChecker.rb
@@ -2,7 +2,23 @@
 
 require_relative "LuhnChecker/version"
 
-module LuhnChecker
-  class Error < StandardError; end
-  # Your code goes here...
+module LuhnCheckerLib
+  VERSION = '0.1.0'
+
+  #Checks validity of bank card number
+  def CheckBankCardNumber(bankcardnumber)
+    nil
+  end
+  
+  #Checks validity of IMEI
+  def CheckIMEI(imei)
+    nil
+  end
+
+  #Checks validity of ICCID
+  def CheckICCID(iccID)
+    nil
+  end
+
+
 end
